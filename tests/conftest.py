ace5f75d4a6d01b4d1baa4a5c7a365d84c443db0
QARoronoa <issa.kamara@ymail.com>
2025-09-20 13:41:22 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,16 +4,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope="function")
-def setup(base_url):  # fourni par pytest-base-url
+def setup(base_url):
     opts = Options()
     opts.add_argument("--headless=new")
     opts.add_argument("--no-sandbox")
     opts.add_argument("--disable-dev-shm-usage")
+
     driver = webdriver.Chrome(options=opts)
-
-    # Ouvre la page de test
-    url = base_url or "https://www.saucedemo.com/"
-    driver.get(url)
-
+    driver.get(base_url or "https://www.saucedemo.com/")
     yield driver
     driver.quit()
